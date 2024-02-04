20be393cda48f9018b9a0b43b332f8509eda424c
Sa Liu <asaliu@ucdavis.edu>
2024-02-03 17:21:37 -0800

    update categories

--- v1/script.js
+++ v1/script.js
@@ -3,7 +3,7 @@ const initialFacts = [
     id: 1,
     text: "React is being developed by Meta (formerly facebook)",
     source: "https://opensource.fb.com/",
-    category: "technology",
+    category: "events",
     votesInteresting: 24,
     votesMindblowing: 9,
     votesFalse: 4,
@@ -14,7 +14,7 @@ const initialFacts = [
     text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
     source:
       "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
+    category: "resources",
     votesInteresting: 11,
     votesMindblowing: 2,
     votesFalse: 0,
@@ -24,7 +24,7 @@ const initialFacts = [
     id: 3,
     text: "Lisbon is the capital of Portugal",
     source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
+    category: "interview",
     votesInteresting: 8,
     votesMindblowing: 3,
     votesFalse: 1,
@@ -33,14 +33,13 @@ const initialFacts = [
 ];
 
 const CATEGORIES = [
-  { name: "technology", color: "#3b82f6" },
-  { name: "science", color: "#16a34a" },
-  { name: "finance", color: "#ef4444" },
-  { name: "society", color: "#eab308" },
-  { name: "entertainment", color: "#db2777" },
-  { name: "health", color: "#14b8a6" },
-  { name: "history", color: "#f97316" },
-  { name: "news", color: "#8b5cf6" },
+  { name: "events", color: "#3b82f6" },
+  { name: "resources", color: "#16a34a" },
+  { name: "interview", color: "#ef4444" },
+  { name: "internship", color: "#eab308" },
+  { name: "referrals", color: "#db2777" },
+  { name: "tech support", color: "#14b8a6" },
+  { name: "others", color: "#8b5cf6" },
 ];
 
 // Selecting DOM elements
